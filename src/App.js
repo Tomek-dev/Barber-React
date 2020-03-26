5b59592417c77c1197f1536290bed2165b876ecd
Tomek-dev <siminiak.tomek@gmail.com>
2020-03-27 00:56:58 +0100

    Init commit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,25 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { Component } from 'react';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component{
+
+
+  componentDidMount() {
+    fetch("")
+    .then(response => response.json)
+    .then(data => {
+        console.log(data);
+        this.setState({ data });
+    });
+  }
+
+  render() {
+    return (
+      <div>
+        Hello World!
+      </div>
+    );
+  }
 }
 
 export default App;
